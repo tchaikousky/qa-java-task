57a0ab2c58854397a97100695e2dd1474851299a
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2021-05-14 11:49:57 -0400

    TT: Initial commit of QA java exercises.

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int x = 7;
+		int y = 10;
+		System.out.println("A small program.");
+		System.out.println(x + y);
+
+	}
+}
---
+++ TestProject/src/com/tchaikousky/TestClass.java
@@ -0,0 +1,14 @@
+package com.tchaikousky;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		exampleMethod();
+
+	}
+	
+	public static void exampleMethod() {
+		System.out.println("String from example method.");
+	}
+
+}
