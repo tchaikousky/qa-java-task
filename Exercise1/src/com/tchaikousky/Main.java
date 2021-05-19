a29c9d7c88b6d513cfcf9ce4763ca535ff0057cc
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2021-05-19 17:11:05 -0400

    TT: changed the access modifiers of the instance variables of the Person class to private. Implemented getters and setters to access those variables.

--- Exercise1/src/com/tchaikousky/Main.java
+++ Exercise1/src/com/tchaikousky/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		LocalDate prishDate = LocalDate.parse("2020-10-15");
 		Person jim = new Person("Jim", 23, "M");
-		Book prish = new Book("The Prish: Unraveled", "Porrcia Thomas", 321, prishDate);
+		Book prish = new Book("The Prish: Unrivaled", "Porrcia Thomas", 321, prishDate);
 		
 		jim.read(prish);
 	}
--- Exercise1/src/com/tchaikousky/Person.java
+++ Exercise1/src/com/tchaikousky/Person.java
@@ -1,11 +1,11 @@
 package com.tchaikousky;
 
 public class Person {
-	public String name; //
-	public int age; //
-	public String gender; //
-	public boolean isBreathing;
-	public int walkSpeed = 30; // in feet
+	private String name; //
+	private int age; //
+	private String gender; //
+	private boolean isBreathing;
+	private int walkSpeed = 30; // in feet
 // Constructor
 
 	public Person(String name, int age, String gender) {
@@ -13,6 +13,57 @@ public class Person {
 		this.age = age;
 		this.gender = gender;
 	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public boolean isBreathing() {
+		return isBreathing;
+	}
+
+
+	public void setBreathing(boolean isBreathing) {
+		this.isBreathing = isBreathing;
+	}
+
+
+	public int getWalkSpeed() {
+		return walkSpeed;
+	}
+
+
+	public void setWalkSpeed(int walkSpeed) {
+		this.walkSpeed = walkSpeed;
+	}
+
 
 	public void read(Book book) {
 		System.out.println(this.name + " is reading the book titled: " + book.getTitle() + ".");
